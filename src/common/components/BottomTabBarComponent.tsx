b01278ce5af0144549964889fc6303ee3132757b
Anush <heyanush@gmail.com>
2022-03-05 23:17:45 +0530

    Supplied BottomTabBarComponent with the list of used tabs. Removed legacy styles file.

--- src/common/components/BottomTabBarComponent.tsx
+++ src/common/components/BottomTabBarComponent.tsx
@@ -7,6 +7,7 @@ import {
   TouchableHighlight,
   View,
 } from "react-native";
+import { ROUTE_LIST, ROUTE_TITLE } from "../../core/constants/routes";
 
 type TTabData = {
   index: number;
@@ -15,8 +16,38 @@ type TTabData = {
   iconSelected: any;
 };
 
-const TABS_DATA: Array<TTabData> = [];
-
+const TABS_DATA: Array<TTabData> = [
+  {
+    index: 0,
+    title: ROUTE_TITLE[ROUTE_LIST.HOME_TAB],
+    icon: "",
+    iconSelected: "",
+  },
+  {
+    index: 1,
+    title: ROUTE_TITLE[ROUTE_LIST.DEBIT_CARD_TAB],
+    icon: "",
+    iconSelected: "",
+  },
+  {
+    index: 2,
+    title: ROUTE_TITLE[ROUTE_LIST.PAYMENTS_TAB],
+    icon: "",
+    iconSelected: "",
+  },
+  {
+    index: 3,
+    title: ROUTE_TITLE[ROUTE_LIST.CREDIT_TAB],
+    icon: "",
+    iconSelected: "",
+  },
+  {
+    index: 4,
+    title: ROUTE_TITLE[ROUTE_LIST.PROFILE_TAB],
+    icon: "",
+    iconSelected: "",
+  },
+];
 
 const BottomTabBarComponent: React.FC<BottomTabBarProps> = ({
   navigation,
