95202c1aaf235d140cc7fe2eb55b992b32e3bac8
Anush <heyanush@gmail.com>
2022-03-05 23:16:45 +0530

    Moved generalContainer style in the component file itself.

--- src/common/container/generalContainer.tsx
+++ src/common/container/generalContainer.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { View } from "react-native";
-import styles from "./styles";
+import { StyleSheet } from "react-native";
 
 type TProps = {
   children: any;
 };
 
 const GeneralContainer = (props: TProps) => {
-  return <View style={styles.container}>{props.children}</View>;
+  return <View style={style.container}>{props.children}</View>;
 };
 
 export default GeneralContainer;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
