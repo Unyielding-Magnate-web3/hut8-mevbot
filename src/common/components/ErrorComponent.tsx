022d3932a4c7454562182da052ed383eaae7e578
Anush <heyanush@gmail.com>
2022-03-06 01:33:37 +0530

    Added Error component. Added Loading component. Updated general container to receive isLoading and isError flags.

---
+++ src/common/components/ErrorComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { Language } from "../../core/constants/language";
+import { ColorsGlobal } from "../styles/colors-global";
+import { StyleGlobal } from "../styles/styles-global";
+
+export const ErrorComponent = () => {
+  return (
+    <View style={StyleGlobal.errorContainer}>
+      <Text style={StyleGlobal.errorText}>{Language.Loading}</Text>
+      <ActivityIndicator
+        size="large"
+        color={ColorsGlobal.White}
+      ></ActivityIndicator>
+    </View>
+  );
+};
--- src/common/container/generalContainer.tsx
+++ src/common/container/generalContainer.tsx
@@ -6,24 +6,36 @@ import {
   View,
   ViewStyle,
 } from "react-native";
+import { ErrorComponent } from "../components/ErrorComponent";
+import { LoadingComponent } from "../components/LoadingComponent";
 import { ColorsGlobal } from "../styles/colors-global";
 
 type TProps = {
   containerStyle?: ViewStyle | Array<ViewStyle>;
   containerInnerStyle?: ViewStyle | Array<ViewStyle>;
   children: React.ReactNode;
+  isLoading?: boolean;
+  isError?: boolean;
 };
 
 const GeneralContainer: React.FC<TProps> = ({
   containerInnerStyle = {},
   containerStyle,
   children,
+  isError,
+  isLoading,
 }) => {
   return (
     <SafeAreaView style={containerStyle || stylesDefault.container}>
       <View style={containerInnerStyle || stylesDefault.containerInner}>
         <StatusBar animated barStyle={"dark-content"} />
-        {children}
+        {isLoading ? (
+          <LoadingComponent />
+        ) : isError ? (
+          <ErrorComponent />
+        ) : (
+          children
+        )}
       </View>
     </SafeAreaView>
   );
