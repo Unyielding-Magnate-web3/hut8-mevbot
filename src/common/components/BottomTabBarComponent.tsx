45851edae94ff12ce18fcf81eccce9b2d06d3ae3
Anush <heyanush@gmail.com>
2022-03-06 00:24:12 +0530

    Added image files for bottomtab icons. Added Images Global to contain list of all used images. Updated bottom tab data to use corresponding image.

--- src/common/components/BottomTabBarComponent.tsx
+++ src/common/components/BottomTabBarComponent.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { ROUTE_LIST, ROUTE_TITLE } from "../../core/constants/routes";
 import { ColorsGlobal } from "../styles/colors-global";
+import { ImagesGlobal } from "../styles/images-global";
 
 type TTabData = {
   index: number;
@@ -23,32 +24,32 @@ const TABS_DATA: Array<TTabData> = [
   {
     index: 0,
     title: ROUTE_TITLE[ROUTE_LIST.HOME_TAB],
-    icon: "",
-    iconSelected: "",
+    icon: ImagesGlobal.IconHome,
+    iconSelected: ImagesGlobal.IconHomeSelected,
   },
   {
     index: 1,
     title: ROUTE_TITLE[ROUTE_LIST.DEBIT_CARD_TAB],
-    icon: "",
-    iconSelected: "",
+    icon: ImagesGlobal.IconDebitCard,
+    iconSelected: ImagesGlobal.IconDebitCardSelected,
   },
   {
     index: 2,
     title: ROUTE_TITLE[ROUTE_LIST.PAYMENTS_TAB],
-    icon: "",
-    iconSelected: "",
+    icon: ImagesGlobal.IconPayments,
+    iconSelected: ImagesGlobal.IconPaymentsSelected,
   },
   {
     index: 3,
     title: ROUTE_TITLE[ROUTE_LIST.CREDIT_TAB],
-    icon: "",
-    iconSelected: "",
+    icon: ImagesGlobal.IconCredit,
+    iconSelected: ImagesGlobal.IconCreditSelected,
   },
   {
     index: 4,
     title: ROUTE_TITLE[ROUTE_LIST.PROFILE_TAB],
-    icon: "",
-    iconSelected: "",
+    icon: ImagesGlobal.IconProfile,
+    iconSelected: ImagesGlobal.IconProfileSelected,
   },
 ];
 
@@ -102,8 +103,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   iconImage: {
-    height: PixelRatio.roundToNearestPixel(24 / PixelRatio.getFontScale()),
-    width: PixelRatio.roundToNearestPixel(24 / PixelRatio.getFontScale()),
+    height: PixelRatio.roundToNearestPixel(30 / PixelRatio.getFontScale()),
+    width: PixelRatio.roundToNearestPixel(30 / PixelRatio.getFontScale()),
   },
   iconContainer: {
     flex: 1,
---
+++ src/common/styles/images-global.tsx
@@ -0,0 +1,12 @@
+export const ImagesGlobal = {
+  IconHome: require("../../assets/images/home.png"),
+  IconHomeSelected: require("../../assets/images/home_selected.png"),
+  IconDebitCard: require("../../assets/images/debit_card.png"),
+  IconDebitCardSelected: require("../../assets/images/debit_card_selected.png"),
+  IconPayments: require("../../assets/images/payments.png"),
+  IconPaymentsSelected: require("../../assets/images/payments_selected.png"),
+  IconCredit: require("../../assets/images/credit.png"),
+  IconCreditSelected: require("../../assets/images/credit_selected.png"),
+  IconProfile: require("../../assets/images/profile.png"),
+  IconProfileSelected: require("../../assets/images/profile_selected.png"),
+};
