5db6b5f7915ec66badb76f4b2177bfd9eb00fabc
Anush <heyanush@gmail.com>
2022-03-06 10:15:47 +0530

    Added state for weekly spending limit option in Debit card tab.

--- src/screens/BottomTabs/DebitCardTab/index.tsx
+++ src/screens/BottomTabs/DebitCardTab/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
 import { ScrollView, View } from "react-native";
+import { RootStackParamList } from "../../../AppContainer";
 import AvailableBalance from "../../../common/components/AvailableBalance";
 import CardComponent from "../../../common/components/CardComponent";
 import HeaderStyle1Component from "../../../common/components/HeaderType1Component";
@@ -9,8 +11,32 @@ import { eMenuItemType, eMenuItemValue } from "../../../core/constants/enums";
 import { Language } from "../../../core/constants/language";
 import MenuItem from "./components/MenuItem";
 import styles from "./styles";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { ROUTE_LIST } from "../../../core/constants/routes";
 
 const DebitCardTab = () => {
+  const currentWeeklyLimit = 10;
+
+  type NavigationProps = StackNavigationProp<
+    RootStackParamList,
+    ROUTE_LIST.BOTTOM_TAB
+  >;
+  const navigation = useNavigation<NavigationProps>();
+
+  const [isWeeklyEnabled, setIsWeeklyEnabled] = useState(
+    currentWeeklyLimit ? true : false
+  );
+
+  const onMenuItemWeeklyPress = () => {
+    if (!isWeeklyEnabled) {
+      setIsWeeklyEnabled(true);
+      // navigation.navigate(ROUTE_LIST.SPENDING_LIMIT_SCREEN);
+    } else {
+      // dispatch(updateUserWeeklyLimit(0));
+      setIsWeeklyEnabled(false);
+    }
+  };
+
   return (
     <GeneralContainer containerInnerStyle={styles.container}>
       <HeaderStyle1Component title="Debit Card">
@@ -34,12 +60,16 @@ const DebitCardTab = () => {
               value={eMenuItemValue.None}
             ></MenuItem>
             <MenuItem
-              onPress={() => {}}
+              onPress={onMenuItemWeeklyPress}
               icon={ImagesGlobal.IconMenuDebitWeekly}
               title={Language.MenuItemTitleDebitWeeklySpending}
               description={Language.MenuItemDescriptionDebitWeeklySpending}
               type={eMenuItemType.Toggle}
-              value={eMenuItemValue.Inactive}
+              value={
+                isWeeklyEnabled
+                  ? eMenuItemValue.Active
+                  : eMenuItemValue.Inactive
+              }
             ></MenuItem>
             <MenuItem
               icon={ImagesGlobal.IconMenuDebitFreeze}
