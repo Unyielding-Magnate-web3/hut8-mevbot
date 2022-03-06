edff482e009df1619624bf5a9911129386cabe66
Anush <heyanush@gmail.com>
2022-03-06 16:09:24 +0530

    Added action to fetch and set the member data from API

--- src/redux/actions/generalActions.tsx
+++ src/redux/actions/generalActions.tsx
@@ -1,10 +1,29 @@
+import { MemberData } from "../../core/models/MemberData";
 import * as actionsTypes from "../actionTypes/generalActionTypes";
 
 export function actionSetSpendingLimit(
-    spendingLimit: number,
-  ): actionsTypes.SetSpendingLimitActionType {
-    return {
-      type: actionsTypes.SET_SPENDING_LIMIT,
-      spendingLimit,
-    };
-  }
+  spendingLimit: number
+): actionsTypes.SetSpendingLimitActionType {
+  return {
+    type: actionsTypes.SET_SPENDING_LIMIT,
+    spendingLimit,
+  };
+}
+
+export function actionSetMemberData(
+  member: MemberData
+): actionsTypes.SetMemberDataActionType {
+  return {
+    type: actionsTypes.SET_MEMBER_DATA,
+    member,
+  };
+}
+
+export function actionLoadMemberData(
+  fail: (status: string) => void
+): actionsTypes.LoadMemberDataActionType {
+  return {
+    type: actionsTypes.LOAD_MEMBER_DATA,
+    fail,
+  };
+}
